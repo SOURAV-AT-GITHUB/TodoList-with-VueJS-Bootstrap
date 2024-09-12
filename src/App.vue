63e9cbb4a4e6c3d25f93b41457298303a8b2a99c
Sourav <souravpramanick1997@gmail.com>
2024-09-13 00:14:47 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log(event)
+}
+</script>
+
+<template>
+ 
+<form @submit="handleSubmit">
+  <input type="text" placeholder="Enter Task">
+  <button class="btn btn-success">Add Task</button>
+
+</form>
+</template>
+
+<style scoped>
+
+</style>
